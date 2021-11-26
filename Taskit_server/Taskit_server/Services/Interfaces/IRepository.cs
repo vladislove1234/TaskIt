03cd8ed28b0448c82ce64e7618071f36cbb83ea6
vladislove1234 <40027853+vladislove1234@users.noreply.github.com>
2021-11-26 18:28:57 +0200

    Added server

---
+++ Taskit_server/Taskit_server/Services/Interfaces/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Taskit_server.Services.Interfaces
+{
+    public interface IRepository<T>  
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int Id);
+        Task<long> Add(T entity);
+    }
+}
---
+++ Taskit_server/Taskit_server/Services/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Taskit_server.Services.Interfaces;
+
+namespace Taskit_server.Services
+{
+    public class Repository<T> : IRepository<T> 
+    {
+        public Repository(DataContext context)
+        {
+
+        }
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<long> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
